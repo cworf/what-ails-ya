bb27c41900502be5af96fc43a1e6c7b6b83d77e7
cworf <colin.worf@gmail.com>
2018-01-12 09:09:37 -0800

    create loop to parse front end data into quryString

--- dev/js/logic.js
+++ dev/js/logic.js
@@ -1,10 +1,13 @@
-export function findDoctor(filterName, filterValue){
+export function findDoctor(filters){ //filters is an object of various filter objects
 	const apiKey = require('./../../.env').apiKey;
-	for (var i = 0; i < filterName.length; i++) {
-		filterName[i];
+	const filterArr = [];
+	for (var key in filters) { //loop through filter object and concat its key-value into the filter array
+		if (filters.hasOwnProperty(key)) {
+			filterArr.push(filters[key].name + "=" + filters[key].value);
+		}
 	}
-	const filterString = ``
-	const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523%2C-122.676%2C100&${filterString}&skip=0&limit=10&user_key=${apiKey}`);
+	const queryString = filterArr.join('&'); //create query string from filter array
+	const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.523%2C-122.676%2C100&${queryString}&skip=0&limit=10&user_key=${apiKey}`);
 	xhr.done(function(results){
 
 	});
